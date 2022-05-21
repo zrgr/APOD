a351ff2994ce1970f557707162e946bd9667fb0d
Zak <zrodger7@gmail.com>
2022-05-21 19:30:16 +0100

    Updated model

--- app/src/main/java/com/example/apod/models/Apod.kt
+++ app/src/main/java/com/example/apod/models/Apod.kt
@@ -1,12 +1,14 @@
 package com.example.apod.models
 
+import com.squareup.moshi.Json
+
 data class Apod(
     val copyright: String,
     val date: String,
     val explanation: String,
     val hdurl: String,
-    val media_type: String,
-    val service_version: String,
+    @Json(name = "media_type") val mediaType: String,
+    @Json(name = "service_version") val serviceVersion: String,
     val title: String,
     val url: String
 )
