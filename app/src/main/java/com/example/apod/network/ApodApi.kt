ec935fd1828e636b114c73217818dff2cb1d4a49
Zak <zrodger7@gmail.com>
2022-05-21 22:38:23 +0100

    Added network/api files

---
+++ app/src/main/java/com/example/apod/network/ApodApi.kt
@@ -0,0 +1,15 @@
+package com.example.apod.network
+
+import com.example.apod.models.Apod
+import com.example.apod.utils.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApodApi {
+    @GET
+    suspend fun getApod(
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<Apod>
+}
---
+++ app/src/main/java/com/example/apod/network/RetrofitInstance.kt
@@ -0,0 +1,25 @@
+package com.example.apod.network
+
+import com.example.apod.utils.Constants.Companion.BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+        private val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        private val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(BASE_URL)
+            .build()
+
+        val api by lazy {
+            retrofit.create(ApodApi::class.java)
+        }
+    }
+}
