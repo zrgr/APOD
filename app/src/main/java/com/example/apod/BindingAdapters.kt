b927f5140a7b7b5fadd67c75167961e6cff804fa
Zak <zrodger7@gmail.com>
2022-06-08 21:30:46 +0100

    Added adapter

--- app/src/main/java/com/example/apod/BindingAdapters.kt
+++ app/src/main/java/com/example/apod/BindingAdapters.kt
@@ -3,7 +3,10 @@ package com.example.apod
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.apod.models.Apod
+import com.example.apod.ui.GalleryAdapter
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -11,4 +14,11 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         imgView.load(imgUri)
     }
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<Apod>?) {
+    val adapter = recyclerView.adapter as GalleryAdapter
+    adapter.submitList(data)
 }
