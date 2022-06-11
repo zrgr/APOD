00f3e5f9a6026373199971592bca51cc3a3b1eb8
Zak <zrodger7@gmail.com>
2022-06-12 00:18:20 +0100

    Updated gallery adapter

--- app/src/main/java/com/example/apod/ui/GalleryAdapter.kt
+++ app/src/main/java/com/example/apod/ui/GalleryAdapter.kt
@@ -2,20 +2,22 @@ package com.example.apod.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.apod.databinding.GalleryItemBinding
 import com.example.apod.models.Apod
 
-class GalleryAdapter: ListAdapter<Apod, GalleryAdapter.ApodViewHolder>(DiffCallback) {
+class GalleryAdapter(val clickListener: ApodListener): ListAdapter<Apod, GalleryAdapter.ApodViewHolder>(DiffCallback) {
 
     class ApodViewHolder(private var binding: GalleryItemBinding
     ):
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(apod: Apod) {
+        fun bind(clickListener: ApodListener, apod: Apod) {
             binding.photo = apod
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
     }
@@ -41,6 +43,10 @@ class GalleryAdapter: ListAdapter<Apod, GalleryAdapter.ApodViewHolder>(DiffCallb
 
     override fun onBindViewHolder(holder: GalleryAdapter.ApodViewHolder, position: Int) {
         val apod = getItem(position)
-        holder.bind(apod)
+        holder.bind(clickListener, apod)
     }
+}
+
+class ApodListener(val clickListener: (apod: Apod) -> Unit) {
+    fun onClick(apod: Apod) = clickListener(apod)
 }
