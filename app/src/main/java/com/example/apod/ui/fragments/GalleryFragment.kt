a024f9ee3be89cda135e003fe89dc0be08eee4c1
zrgr <zrodger7@gmail.com>
2023-04-14 11:18:59 +0700

    Added spinner

--- app/src/main/java/com/example/apod/ui/fragments/GalleryFragment.kt
+++ app/src/main/java/com/example/apod/ui/fragments/GalleryFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -17,6 +19,7 @@ import com.example.apod.ui.GalleryAdapter
 class GalleryFragment : Fragment() {
 
     private val viewModel: ApodViewModel by activityViewModels()
+    lateinit var binding: FragmentGalleryBinding
 
     /**
      * Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
@@ -26,7 +29,7 @@ class GalleryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentGalleryBinding.inflate(inflater)
+        binding = FragmentGalleryBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
@@ -40,6 +43,26 @@ class GalleryFragment : Fragment() {
                 .navigate(R.id.action_galleryFragment_to_apodFragment)
         })
 
+
+        populateSpinner()
+
+
         return binding.root
+    }
+
+    private fun populateSpinner() {
+        val spinner: Spinner = binding.timePeriodSpinner
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.time_period_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
 }
