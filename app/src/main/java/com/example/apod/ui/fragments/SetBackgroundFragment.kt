553eda713ea382fd1b03f68fc46b9ba034813da8
zrgr <zrodger7@gmail.com>
2023-04-19 22:10:55 +0700

    Tidy up

--- app/src/main/java/com/example/apod/ui/fragments/SetBackgroundFragment.kt
+++ app/src/main/java/com/example/apod/ui/fragments/SetBackgroundFragment.kt
@@ -12,6 +12,7 @@ import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import coil.load
+import coil.request.ImageRequest
 import com.example.apod.ApodViewModel
 import com.example.apod.ImageStatus
 import com.example.apod.R
